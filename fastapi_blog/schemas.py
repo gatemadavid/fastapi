dd9d35cd2d1176117979477ceaf28184d7fe07d1
david <david@davie.gatemadavid.com>
2026-07-02 23:03:53 +0300

    added delete and update routes to user and posts

--- fastapi_blog/schemas.py
+++ fastapi_blog/schemas.py
@@ -1,18 +1,47 @@
-from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+
+class UserBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    username: str = Field(min_length=1, max_length=50, description="The username of the user")
+    email: EmailStr = Field(max_length=120, description="The email address of the user")
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_file: str | None
+    image_path: str
+
+class userUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50, description="The username of the user")
+    email: EmailStr | None = Field(default=None, max_length=120, description="The email address of the user")
+    image_file: str | None = Field(default=None, min_length=1, max_length=200,description="The image file name of the user")
 
 class PostBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     title: str = Field(min_length=1, max_length=100, description="The title of the post")
     content: str = Field(min_length=1,description="The content of the post")
-    author:str = Field(min_length=1, max_length=50, description="The author of the post")
+    
 
 class PostCreate(PostBase):
-    pass
+    user_id:int
+
+class PostUpdate(BaseModel):
+    
+    title: str | None = Field(default=None, min_length=1, max_length=100, description="The title of the post")
+    content: str | None = Field(default=None, min_length=1, description="The content of the post")
 
 class PostResponse(PostBase):
-    model_config = ConfigDict(from_attributes=True)
-
+    
     id:int
-    date_posted:str
+    user_id:int
+    likes:int
+    date_posted:datetime
+    author: UserResponse
     
