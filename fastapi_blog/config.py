e1ddedab9958212b95aa875ab50fd85db03f05a0
david <david@davie.gatemadavid.com>
2026-07-05 12:12:40 +0300

    changed config

--- fastapi_blog/config.py
+++ fastapi_blog/config.py
@@ -11,4 +11,15 @@ class Settings(BaseSettings):
     max_upload_size: int = 5 * 1024 * 1024  # 5 MB
     posts_per_page: int = 10
 
+    rest_token_expire_minutes: int = 15
+    mail_server: str = 'localhost'
+    mail_port: int = 587
+    mail_username: str = ''
+    mail_password: SecretStr = SecretStr('')
+    mail_from: str = 'noreply@gmail.com'
+    mail_use_tls: bool = True
+
+    frontend_url: str = 'http://localhost:8000'
+
+
 settings = Settings()   
